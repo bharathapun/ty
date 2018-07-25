ab7bb4b342765168478c94ea95f7656ecfe51502
bharathapun <40815908+bharathapun@users.noreply.github.com>
2018-07-25 09:19:05 +0530

    Update ty.c

--- ty.c
+++ ty.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
-#include <string.h>
-int main(int argc, char *argv[])
+#include<string.h>
+int main()
 {
-  size_t maxlen = 0, len;
-  int j;
-  int long;
- for (j=1; j<argc;i++)
- {
-   len = strlen(argv[j]);
-     if (len > maxlen) 
-       long = argv[j];
-  }
- printf("longest string is %s. \n", long);
- return 0;
+	int a1,b1,max=0,i;
+	char a[100],b[100];
+	printf("\nEnter the first string:  ");
+	scanf("%s",a);
+	printf("\nEnter the second string:  ");
+	scanf("%s",b);
+	a1=strlen(a);
+	b1=strlen(b);
+	if(a1>b1)
+	{
+		max=a1;
+	}
+	else
+	{
+		max=b1;
+	}
+	for(i=0;i<max;i++)
+	{
+		if(a[i]==b[i])
+		{
+			printf("%c",a[i]);
+		}
+		else
+		{
+			break;
+		}
+	}
+	return 0;
 }
